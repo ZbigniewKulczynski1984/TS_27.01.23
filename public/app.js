d905806d98af137ed6cc0c74061b645f8d26be47
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-31 18:39:55 +0100

    code added

---
+++ public/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const anchor = document.querySelector('a');
+const form = document.querySelector('.new-item-form');
+// if(anchor) {
+//     console.log(anchor.href);
+// }
+const type = document.querySelector('#type');
+const toForm = document.querySelector('#toform');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+const anchor = document.querySelector('a')!;
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+// if(anchor) {
+//     console.log(anchor.href);
+// }
+
+const type = document.querySelector('#type') as HTMLSelectElement
+const toForm = document.querySelector('#toform') as HTMLInputElement
+const details = document.querySelector('#details') as HTMLInputElement
+const amount = document.querySelector('#amount') as HTMLInputElement
