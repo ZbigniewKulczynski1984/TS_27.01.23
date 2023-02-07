bbd30e847a6da518cf874fbc465ece44e41399fb
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-07 16:43:41 +0100

    code added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Invoice } from './classes/Invoice.js';
+import { Invoice } from './classes/Invoices';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
