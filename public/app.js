c501eeb484195f3078bbfb44aebc336312996a6c
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-08 14:31:46 +0100

    code added

--- public/app.js
+++ public/app.js
@@ -1,21 +1,11 @@
-"use strict";
-class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/Invoices.js';
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 invoices.forEach(inv => {
-    console.log(inv.client, inv.details, inv.amount, inv.format());
+    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
 });
 const form = document.querySelector('.new-item-form');
 console.log(form.children);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { Invoice } from './classes/Invoices';
+import { Invoice } from './classes/Invoices.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
