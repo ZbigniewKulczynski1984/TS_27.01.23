15e93170a76d867be8fd6cf7af39a20334494d5b
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-02-08 14:53:42 +0100

    code added

--- public/app.js
+++ public/app.js
@@ -1,28 +1,36 @@
 import { Invoice } from './classes/Invoices.js';
 import { Payment } from './classes/Payments.js';
-let docOne;
-let docTwo;
-docOne = new Invoice('yoshi', 'webwork', 250);
-docTwo = new Payment('mario', 'plumbing', 200);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
+import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(...values);
+    }
+    else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, 'end');
 });
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+let student;
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,9 @@
 import { Invoice } from './classes/Invoices.js';
 import { Payment } from './classes/Payments.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice('yoshi', 'webwork', 250);
-docTwo = new Payment('mario', 'plumbing', 200);
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo);
-
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach((inv) => {
-	console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
@@ -32,8 +11,36 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
-form.addEventListener('submit', (e: Event) => {
-	e.preventDefault();
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
-	console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice') {
+    doc = new Invoice(...values);
+  } else {
+    doc = new Payment(...values);
+  }
+  
+  list.render(doc, type.value, 'end');
 });
+
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
